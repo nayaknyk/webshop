5d8d1eff1b63de0a915e7444516ddb7979ceaab8
nayaknyk <30729324+nayaknyk@users.noreply.github.com>
2019-06-24 02:03:02 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -7,23 +7,3 @@ $(document).ready(function() {
         $('#cart').html(data);
     });
 });
-
-// When an order button is clicked: get url from element and run the action
-/*$('.order').click(function(event) {
-
-    // Run the url provided in the a link
-    $.ajax({
-        url: $(this).attr('href'),
-    })
-    .done(function(data) {
-        // if the action was succesfull, update the cart with the server data
-        $('#cart').html(data);
-        // open the cart for the user so he can see an item was added
-        $('.cart').css('right', 0);
-        // close the cart after x milliseconds to get it out of the way
-        setTimeout(function() {
-            $('.cart').css('right', -385);
-        }, 300);
-    });
-});
-*/
