a2f2977873390d738917b10a2a25deae960df63e
nayaknyk <30729324+nayaknyk@users.noreply.github.com>
2019-06-25 19:16:06 +0530

    error pages

---
+++ 400.php
@@ -0,0 +1,3 @@
+<title>Error 400</title>
+<h3>Error 400: Bad Request</h3>
+Your client has issued a malformed or illegal request, which unfortunately sent this server into despair and desolation.
---
+++ 404.php
@@ -0,0 +1,4 @@
+<title>Error 404</title>
+<h1>Error 404: Page not Found</h1>
+<b><?= $_SERVER['REQUEST_URI']; ?> does not exist.<br><br>
+Make sure you typed the right url, and maybe try turning it off/on again, eh?
